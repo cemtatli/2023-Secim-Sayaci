520c9fc44572423ab73c0acae1218a2f06e0bdb5
Cem Tatlı <cemtatli3535@gmail.com>
2023-01-18 20:40:08 +0300

    update: add footer

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Geri saydığımız tarih
-let countDownDate = new Date("May 14, 2023 09:05:00").getTime();
+let countDownDate = new Date("May 14, 2023 00:00:00").getTime();
 
 // Geri sayımı her 1 saniyede bir güncelleme
 let x = setInterval(function () {
@@ -14,16 +14,16 @@ let x = setInterval(function () {
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
   // DOM'a yazdırma işlemi
-  document.getElementById("days").innerHTML = `${days} <span class="text-5xl md:text-2xl">gün</span>`;
-  document.getElementById("hours").innerHTML = `${hours} <span class="text-5xl md:text-2xl">saat</span>`;
-  document.getElementById("minutes").innerHTML = `${minutes} <span class="text-5xl md:text-2xl">dakika</span>`;
-  document.getElementById("seconds").innerHTML = `${seconds} <span class="text-5xl md:text-2xl">saniye</span>`;
+  document.getElementById("days").innerHTML = `${days} <span class="text-5xl md:text-2xl"> gün</span>`;
+  document.getElementById("hours").innerHTML = `${hours} <span class="text-5xl md:text-2xl"> saat</span>`;
+  document.getElementById("minutes").innerHTML = `${minutes} <span class="text-5xl md:text-2xl"> dakika</span>`;
+  document.getElementById("seconds").innerHTML = `${seconds} <span class="text-5xl md:text-2xl"> saniye</span>`;
   // Geri sayım bitince yazılacak metin
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "GÖZÜMÜZ AYDIN İNŞ :(";
+    document.getElementById("countdown").innerHTML = "Bugün seçim var !";
   }
-}, 1000);
+});
 
 if (
   localStorage.theme === "dark" ||
